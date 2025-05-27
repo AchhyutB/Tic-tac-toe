b98dadd9a263c3794b472f0d13d428e6faeea846
AchhyutB <achhyut.baral03@gmail.com>
2025-05-27 19:53:01 +0545

    created a fully functional Tic-Tac-Toe game using mainly JS

--- course8(Tic Tac Toe).js
+++ course8(Tic Tac Toe).js
@@ -1,33 +1,50 @@
 let boxes=document.querySelectorAll(".box");
-let resetBtn=document.querySelector("#Reset");
+let resetBtn=document.querySelector("#reset-btn");
 let newGameBtn=document.querySelector("#new-btn");
 let msgContainer=document.querySelector(".msg-container");
 let msg=document.querySelector("#msg");
 
-let turnO = true // playerX, playerO
+
+
+
+let turnO = true 
+
+
+
 
 const winPatterns= [
-     [0,1,2],
-     [3,4,5],
+    [0,1,2],
+    [3,4,5],
     [6,7,8],
     [0,3,6],
     [1,4,7],
     [2,5,8],
-    [0,4,2],
+    [0,4,8],
     [2,4,6]
 ];
+
+
 const resetGame=()=>{
 turnO = true;
 enableBoxes();
-}
+msg.innerText="";
+msgContainer.classList.add("hide");
+winLine.style.width = "0";
+winLine.style.transform = "rotate(0deg)";
+};
+
+
+
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         console.log("button was clicked");
-        if(turnO){   //PlayerO
+        if(turnO){ 
             box.innerText="O";
+            box.classList.add("o");
             turnO=false;
-        } else {     //PlayerX
+        } else {     
             box.innerText="X";
+            box.classList.add("x");
         turnO=true;
         }
         box.disabled=true;
@@ -37,34 +54,83 @@ boxes.forEach((box)=>{
     });
 });
 
+
+
+
 const disableBoxes = ()=>{
     for(let box of boxes){
         box.disabled = true;
     }
 };
 
+
+
+
 const enableBoxes = ()=>{
     for(let box of boxes){
-        box.enabled = false;
+        box.disabled = false; 
+        box.innerText="";
     }
 };
 
-const showWinner = (winner) => {
+
+
+const winLine = document.querySelector("#win-line");
+
+const showWinLine = (pattern) => {
+    const lineStyles = {
+        "0,1,2": { top: "16.5%", left: "0%", width: "100%", rotate: "0deg" },
+        "3,4,5": { top: "49.5%", left: "0%", width: "100%", rotate: "0deg" },
+        "6,7,8": { top: "82.5%", left: "0%", width: "100%", rotate: "0deg" },
+        "0,3,6": { top: "0%", left: "16.5%", width: "100%", rotate: "90deg" },
+        "1,4,7": { top: "0%", left: "49.5%", width: "100%", rotate: "90deg" },
+        "2,5,8": { top: "0%", left: "82.5%", width: "100%", rotate: "90deg" },
+        "0,4,8": { top: "0%", left: "0%", width: "141%", rotate: "45deg" },
+        "2,4,6": { top: "0%", left: "0%", width: "141%", rotate: "-45deg" },
+    };
+
+    const key = pattern.join(",");
+    const style = lineStyles[key];
+
+    if (style) {
+        winLine.style.top = style.top;
+        winLine.style.left = style.left;
+        winLine.style.transform = `rotate(${style.rotate})`;
+        winLine.style.width = style.width;
+    }
+};
+
+
+
+
+const showWinner = (winner,pattern) => {
     msg.innerText=`Congratulations,Winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
+    showWinLine(pattern);
 };
-const checkWinner=()=>{
-     for(let pattern of winPatterns){
-        let pos1Val = boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                console.log("winner",pos1Val);
-                showWinner(pos1Val);
-            }
+
+
+
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let [i, j, k] = pattern;
+        let pos1Val = boxes[i].innerText;
+        let pos2Val = boxes[j].innerText;
+        let pos3Val = boxes[k].innerText;
+
+        if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            showWinner(pos1Val,pattern);
+            return;
         }
-     }
+    }
 };
+
+
+
+
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
